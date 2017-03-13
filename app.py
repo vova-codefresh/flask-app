971afa9cc2f164468a17edf320c744ef0980a143
Vladimir <vladimir@codefresh.io>
2017-03-13 15:56:06 +0200

    small fix

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello():
-    cmd = ["ls"," -l"]
+    cmd = ["ls","-l"]
     p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
                             stderr=subprocess.PIPE,
                             stdin=subprocess.PIPE)
