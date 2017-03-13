0631ceb4f03a2a33e837ac91cbd37adc4f4fb157
Vladimir <vladimir@codefresh.io>
2017-03-13 15:50:44 +0200

    list files

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import subprocess
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
 
-if __name__ == "__main__":
+def hello():
+    cmd = ["ls"," -l"]
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            stdin=subprocess.PIPE)
+    out,err = p.communicate()
+    return out
+if __name__ == "__main__" :
     app.run(host='0.0.0.0')
